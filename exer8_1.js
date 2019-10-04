3c02002f79e6b905f42c63aaf874d1f8288f9bb4
Piotr Kobierski <34033397+piotrkobierski7@users.noreply.github.com>
2019-10-04 17:17:44 +0200

    Create exer8_1.js

---
+++ exer8_1.js
@@ -0,0 +1,49 @@
+'use strict';
+
+var output = document.getElementById('output');
+
+output.innerHTML = 'Click the button!' + '<br><br>' + output.innerHTML; 
+
+var button_CtoF = document.getElementById('C-F');
+var button_FtoC = document.getElementById('F-C');
+
+var temperatureC,temperatureF;
+
+function checkInfo(temperatureC){
+  	if(temperatureC < 0){ 
+    	return ' At this temperature water freezes';
+	}else if(temperatureC < 100){
+  		return ' At this temperature water is in liquid form';
+	}else{ 
+  		return ' At this temperature water is in gas form';
+  	}
+}
+
+function showInfo(temperatureC){
+	var temperatureF=1.8*temperatureC+32;
+	output.innerHTML = 'Temperature in Celsius degrees is '+temperatureC+ '<br><br>' + 'Temperature in Fahrenheit degrees is ' 
+	+temperatureF+ '<br><br>' + checkInfo(temperatureC) + '<br><br>'+ '***************'  + '<br><br>' + output.innerHTML;
+}
+
+button_CtoF.addEventListener('click', function(){	
+	temperatureC = window.prompt('What is the temperature in Celsius degree?');
+		if (!isNaN(temperatureC)){
+	   		showInfo(temperatureC);
+	  }else{
+	    output.innerHTML = temperatureC +' is not a number'+ '<br><br>'+ output.innerHTML; 
+	  }
+}); 
+
+button_FtoC.addEventListener('click', function(){	
+	temperatureF = window.prompt('What is the temperature in Fahrenheit degree?');
+	if (!isNaN(temperatureF)){
+		temperatureC = (temperatureF - 32)/1.8;
+	   	showInfo(temperatureC);
+	}else{
+	   	output.innerHTML = temperatureC +' is not a number'+ '<br><br>'+ output.innerHTML; 
+	}
+}); 
+
+
+
+
