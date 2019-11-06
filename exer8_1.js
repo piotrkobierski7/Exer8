dc230d20ff5ebbf3289ea5009f5a40b1aa96e749
Piotr Kobierski <34033397+piotrkobierski7@users.noreply.github.com>
2019-11-06 17:06:56 +0100

    Change JS
    1) var temperatureC,temperatureF; spacja po przecinku
    
    2) daj po linii przerwy pomiędzy funkcjami
    
    3) wcięcia - np. linia 11, 13, 15
    
    4) na końcu dokumentu max jedna pusta linia

--- exer8_1.js
+++ exer8_1.js
@@ -5,7 +5,7 @@ var button_CtoF = document.getElementById('C-F');
 var button_FtoC = document.getElementById('F-C');
 var tempOffsetFahr = 32; //static numbers for C->F formula
 var tempMultipleFahr = 1.8; //static numbers for C->F formula
-var temperatureC,temperatureF;
+var temperatureC, temperatureF;
 function checkInfo (temperatureC) {
   	if (temperatureC < 0) { 
     	return ' At this temperature water freezes';
@@ -15,11 +15,13 @@ function checkInfo (temperatureC) {
   		return ' At this temperature water is in gas form';
   	}
 }
+
 function showInfo (temperatureC) {
 	var temperatureF=1.8*temperatureC+32;
 	output.innerHTML = 'Temperature in Celsius degrees is '+temperatureC+ '<br><br>' + 'Temperature in Fahrenheit degrees is ' 
 	+temperatureF+ '<br><br>' + checkInfo(temperatureC) + '<br><br>'+ '***************'  + '<br><br>' + output.innerHTML;
 }
+
 button_CtoF.addEventListener ('click', function() {	
 	temperatureC = window.prompt ('What is the temperature in Celsius degree?');
 		if (!isNaN(temperatureC)){
@@ -28,6 +30,7 @@ button_CtoF.addEventListener ('click', function() {
 	    output.innerHTML = temperatureC +' is not a number'+ '<br><br>'+ output.innerHTML; 
 	  }
 }); 
+
 button_FtoC.addEventListener ('click', function() {	
 	temperatureF = window.prompt ('What is the temperature in Fahrenheit degree?');
 	if (!isNaN (temperatureF)) {
@@ -37,7 +40,3 @@ button_FtoC.addEventListener ('click', function() {
 	   	output.innerHTML = temperatureC +' is not a number'+ '<br><br>'+ output.innerHTML; 
 	}
 }); 
-
-
-
-
